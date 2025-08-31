7a9518785931b58a375cea6bad0ca32067828734
Muhammad Ijaz <muhammad.bese23seecs@seecs.edu.pk>
2025-08-31 16:14:22 -0700

    Update: Added APp header

--- app/(routes)/dashboard/_components/AppHeader.tsx
+++ app/(routes)/dashboard/_components/AppHeader.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
 
-function AppHeader(){
-  return <div>App Header</div>
+const menuOptions = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/home"
+  },
+  {
+    id: 2,
+    name: 'History',
+    path: '/history'
+  },
+  {
+    id: 3,
+    name: 'Pricing',
+    path: '/pricing'
+  },
+  {
+    id: 4,
+    name: 'Profile',
+    path: '/profile'
+  }
+]
+
+
+function AppHeader() {
+  return <div>
+    <Image src={"logo.svg"} alt="logo" width={180} height={180} />
+    App Header</div>
 }
 
 export default AppHeader
