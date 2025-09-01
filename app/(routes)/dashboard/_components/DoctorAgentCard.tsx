4698ccbbff5afe6d4c42f914e49657559f013d80
Muhammad Ijaz <muhammad.bese23seecs@seecs.edu.pk>
2025-08-31 17:12:50 -0700

    Update: Doctors list card

--- app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { IconArrowRight } from '@tabler/icons-react'
 import Image from 'next/image'
 import React from 'react'
 type DoctorAgentType = {
@@ -18,7 +19,7 @@ function DoctorAgentCard({ doctorAgent }: props) {
       <Image src={doctorAgent.image} alt={doctorAgent.specialist} width={200} height={300} className='w-full h-[250px] object-cover rounded-xl' />
       <h2 className='font-bold mt-1'>{doctorAgent.specialist}</h2>
       <p className='line-clamp-2 text-sm text-gray-500'>{doctorAgent.description}</p>
-      <Button>Consult</Button>
+      <Button className='w-full mt-2'>Start Conversation <IconArrowRight /></Button>
     </div>
   )
 }
